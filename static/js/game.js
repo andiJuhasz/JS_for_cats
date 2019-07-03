6dd54dd7ba213a2349e60518dbda53dfc97bea2f
Melath Stofi <melath.stofi@gmail.com>
2019-07-03 14:01:59 +0200

    drag and drop works, but multiple images can be placed ito one cell

--- static/js/game.js
+++ static/js/game.js
@@ -1,5 +1,6 @@
-const barCells = Array.from(document.querySelectorAll('.image-cell'));
+const barCells = Array.from(document.querySelectorAll('.option-cell'));
+for (gameCell of Array.from(document.querySelectorAll('.game-cell'))) {
+    barCells.push(gameCell);
+}
+console.log(barCells);
 dragula(barCells);
-
-
-
