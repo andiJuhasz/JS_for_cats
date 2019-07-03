83868d98fe3aaf3d10447dfb7eb9ddc04d9e04df
Melath Stofi <melath.stofi@gmail.com>
2019-07-03 11:42:32 +0200

    listing images dynamically into game.html/option_bar

--- static/js/game.js
+++ static/js/game.js
@@ -1,6 +1,9 @@
-dragula([document.getElementById('ball')]);
 
-dragula([document.getElementsByClassName('game-cell')]);
-let containersArray = Array.from(htmlCollection);
-dragula(containersArray);
+const gameCells = [].slice.call(document.getElementsByClassName('game-cell'));
+console.log(gameCells);
+dragula(gameCells);
+
+const barCells = [].slice.call(document.getElementsByClassName('option-cell'));
+dragula(barCells);
+
 
