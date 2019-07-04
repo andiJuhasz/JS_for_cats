04a23481f58094898802c2f6c2d3ff5510db6036
Andrea Juhasz <jhszandrea@gmail.com>
2019-07-04 10:20:32 +0200

    add new pic to images

--- server.py
+++ server.py
@@ -11,15 +11,15 @@ def index():
 @app.route('/')
 def game():
     image_list = [
-        'static/images/Screenshot from 2019-03-05 13-28-02.png',
-        'static/images/Screenshot from 2019-03-22 16-46-36.png',
-        'static/images/Screenshot from 2019-03-22 16-47-29.png',
-        'static/images/Screenshot from 2019-03-22 16-51-46.png',
-        'static/images/Screenshot from 2019-03-22 17-09-11.png',
-        'static/images/Screenshot from 2019-04-02 17-10-34.png',
-        'static/images/Screenshot from 2019-04-02 17-31-24.png',
-        'static/images/Screenshot from 2019-04-02 17-34-26.png',
-        'static/images/Screenshot from 2019-04-03 22-20-38.png'
+        'static/images/van/Capture1.JPG',
+        'static/images/van/Capture2.JPG',
+        'static/images/van/Capture3.JPG',
+        'static/images/van/Capture4.JPG',
+        'static/images/van/Capture5.JPG',
+        'static/images/van/Capture6.JPG',
+        'static/images/van/Capture7.JPG',
+        'static/images/van/Capture8.JPG',
+        'static/images/van/Capture9.JPG'
     ]
     return render_template("game.html", row_num=3, col_num=3, image_list=image_list)
 
