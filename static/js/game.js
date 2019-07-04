2a744b4d90e12d71cb9ee24ae6697db8c2f80184
Melath Stofi <melath.stofi@gmail.com>
2019-07-04 14:49:50 +0200

    add win check but it works only one direction

--- static/js/game.js
+++ static/js/game.js
@@ -1,43 +1,29 @@
-function shuffle(array) {
-  var currentIndex = array.length, temporaryValue, randomIndex;
-
-  // While there remain elements to shuffle...
-  while (0 !== currentIndex) {
-
-    // Pick a remaining element...
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex -= 1;
-
-    // And swap it with the current element.
-    temporaryValue = array[currentIndex];
-    array[currentIndex] = array[randomIndex];
-    array[randomIndex] = temporaryValue;
-  }
-
-  return array;
-}
-
-
-function randomizer() {
-    const randomizedBarCells = shuffle(barCells);
-    let counter = 0;
-    for (barCell of document.querySelectorAll('.option-cell')) {
-        barCell.innerHTML = randomizedBarCells[counter];
-        counter++;
-    }
-}
-
-
-function check(event) {
-    const droppedOn = event.target;
-    console.log(event.innerHTML);
+function check() {
     imageSequence = [];
     for (cell of document.getElementsByClassName('game-cell')) {
         imageSequence.push(cell.innerHTML)
     }
-    console.log(imageSequence);
-    if (undefined in imageSequence) {
-        alert("I can appear!")
+    const check_list = [
+        '<img class="image" src="/static/images/van/Capture1.JPG">',
+        '<img class="image" src="/static/images/van/Capture2.JPG">',
+        '<img class="image" src="/static/images/van/Capture3.JPG">',
+        '<img class="image" src="/static/images/van/Capture4.JPG">',
+        '<img class="image" src="/static/images/van/Capture5.JPG">',
+        '<img class="image" src="/static/images/van/Capture6.JPG">',
+        '<img class="image" src="/static/images/van/Capture7.JPG">',
+        '<img class="image" src="/static/images/van/Capture8.JPG">',
+        '<img class="image gu-transit" src="/static/images/van/Capture9.JPG">',
+
+    ];
+
+    let checkCount = 0;
+    for (let i = 0; i < check_list.length; i++) {
+        if (check_list[i] === imageSequence[i]) {
+            checkCount++
+        }
+    }
+    if (checkCount === check_list.length) {
+        alert('Szia Andi:)')
     }
 }
 
@@ -45,7 +31,6 @@ function check(event) {
 function main() {
     const barCells = Array.from(document.querySelectorAll('.option-cell'));
     for (gameCell of Array.from(document.querySelectorAll('.game-cell'))) {
-        gameCell.addEventListener('drop', check);
         barCells.push(gameCell);
     }
     const drake = dragula(barCells);
